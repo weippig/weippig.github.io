c9ab00d1ae06bed183762c582eee61c4f579408a
Helen Huang <helpig88319@gmail.com>
2022-08-26 11:16:52 +0800

    add 1loc post & scroll-to-top button

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,30 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+
+// scroll to top button
+let debounceTimeout;
+let body = document.querySelector('body');
+let scrollingElement = document.scrollingElement;
+
+setScrollClass();
+
+window.addEventListener('scroll', setScrollClass);
+window.addEventListener('resize', setScrollClass);
+
+function setScrollClass() {
+  if (debounceTimeout) {
+    window.cancelAnimationFrame(debounceTimeout);
+  }
+
+  debounceTimeout = window.requestAnimationFrame(function () {
+    let scrollTop = scrollingElement.scrollTop;
+    let scrollHeight = scrollingElement.scrollHeight;
+    let innerHeight = window.innerHeight;
+    let scrollBottom = scrollHeight - innerHeight - scrollTop;
+
+    body.classList.toggle('at-top', scrollTop < 48);
+    body.classList.toggle('at-bottom', scrollBottom < 48);
+  });
+}
